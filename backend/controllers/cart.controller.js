e9d3cbad3d62148ebb5bed30f5bd4f16f7f6c531
kamal-sethi <kamalsethi018@gmail.com>
2025-01-18 14:21:39 +0530

    created controller for deleting a product from cart of the user

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -19,3 +19,20 @@ export const addToCart = async (req, res) => {
     res.status(500).json({ message: "internal server error" });
   }
 };
+
+export const removeAllFromCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+
+    const user = req.user;
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item !== productId);
+    }
+    await user.save();
+    res.json(user.cartItems);
+
+    await user.cartItems.find((item) => item.id === productId);
+  } catch (error) {}
+};
