9b03e4d6a63a3d8bf66b18d445fad5c0aa70636f
kamal-sethi <kamalsethi018@gmail.com>
2025-01-18 14:12:35 +0530

    created controller for adding product in the cart for user

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,5 +1,19 @@
 export const addToCart = async (req, res) => {
   try {
+    const { productId } = req.body;
+
+    const user = req.user;
+
+    const existingItem = await user.cartItems.find(
+      (item) => item.id === productId
+    );
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      user.cartItems.push(productId);
+    }
+    await user.save();
+    res.json(cartItems);
   } catch (error) {
     console.log("error in add to cart controller", error.message);
     res.status(500).json({ message: "internal server error" });
