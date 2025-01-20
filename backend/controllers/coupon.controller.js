9df488aeb45e5527431b8c8a91dee1f60c37121c
kamal-sethi <kamalsethi018@gmail.com>
2025-01-20 11:54:44 +0530

    created controller for checking whether the coupon code sent by user is valid or not

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -11,3 +11,33 @@ export const getCoupon = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const getValidateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+
+    const coupon = await Coupon.findOne({
+      code: code,
+      isActive: true,
+      userId: req.user._id,
+    });
+
+    if (!coupon) {
+      return res.status(400).json({ message: "Coupon not found" });
+    }
+
+    if (coupon.expirationDate < new Date()) {
+      (coupon.isActive = false), await coupon.save();
+      return res.status(400, json({ message: "coupon expired" }));
+    }
+
+    res.json({
+      message: "coupon is valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log("error in coupon controller ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
