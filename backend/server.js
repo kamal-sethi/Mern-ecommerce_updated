d68cb5dc62980898fb4f560aa7ffe8c8149fad1e
kamal-sethi <kamalsethi018@gmail.com>
2025-01-20 12:28:53 +0530

    created a mongoose model for storing orders of the user and created  a payments route also

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import productsRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupen.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -18,6 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 app.listen(PORT, () => {
   console.log("Server is running on port 5000");
   connectDB();
