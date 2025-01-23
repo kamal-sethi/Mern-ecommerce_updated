dd07cac487a8ef1a79d62d08be18f812a62e0969
kamal-sethi <kamalsethi018@gmail.com>
2025-01-23 12:42:31 +0530

    created a controller for getting data for analytics and for showing data in chart for thr admin

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -5,6 +5,16 @@ import User from "../models/user.model.js";
 export const getAnalytics = async (req, res) => {
   try {
     const AnalyticsData = await getAnalyticsData();
+
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    const dailySalesData = await getDailySalesData(startDate, endDate);
+
+    res.json({
+      AnalyticsData,
+      dailySalesData,
+    });
   } catch (error) {
     console.log("error in get analytics controller", error.message);
     res.status(500).json({ message: "Internal server error" });
@@ -37,3 +47,48 @@ const getAnalyticsData = async () => {
     totalRevenue,
   };
 };
+
+const getDailySalesData = async (startDate, endDate) => {
+  const dailySalesData = await Order.aggregate([
+    {
+      $match: {
+        // it is like a filter
+        createdAt: {
+          $gte: startDate,
+          $lte: endDate,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+        sales: { $sum: 1 },
+        revenue: { $sum: "$totalAmount" },
+      },
+    },
+    { $sort: { id: 1 } },
+  ]);
+
+  const dateArray = getDatesInRange(startDate, endDate);
+  return dateArray.map((date) => {
+    const foundData = dailySalesData.find((item) => item._id === date);
+
+    return {
+      date,
+      sales: foundData?.sales || 0,
+      revenue: foundData?.revenue || 0,
+    };
+  });
+};
+
+function getDatesInRange(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    dates.push(currentDate.toISOString().split("T")[0]);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
