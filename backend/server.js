171d92a175d0e4a00dda1ce026a9647b2c574f3e
kamal-sethi <kamalsethi018@gmail.com>
2025-01-23 11:49:16 +0530

    created a boiler plate for analytics routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productsRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupen.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -20,6 +21,7 @@ app.use("/api/products", productsRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 app.listen(PORT, () => {
   console.log("Server is running on port 5000");
   connectDB();
